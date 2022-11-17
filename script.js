811f41b780b4a78b659903f4ce274348b263cdcc
fabianlgn <77482232+fabianlgn@users.noreply.github.com>
2022-11-17 19:38:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function book(booking1) {
+
+
+        document.getElementById(booking1).style.display = 'flex';
+
+}
+function closebook(booking1) {
+
+
+    document.getElementById(booking1).style.display = 'none';
+
+}
+
+
+
